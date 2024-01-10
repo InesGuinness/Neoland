dbb0755d2a54cb38ae3e262c77547761cea300ae
ines medem <inesmedem93@gmail.com>
2024-01-10 10:23:41 +0100

    Iteration#1

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -1,4 +1,4 @@
-/*
+/* ITERACIÓN 1
 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
@@ -20,9 +20,9 @@ let y = 10
 //1.4 
 
 z = h + y 
-console.log(z)
+//console.log(z)
 
-/* 
+/* ITERACIÓN 2
 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = {name: 'Jack Sparrow', age: 10};
 
@@ -64,10 +64,97 @@ const character = {name: 'Jack Sparrow', age: 10};
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
-console.log(toy1.price + toy2.price)
+//console.log(toy1.price + toy2.price)
 
-//1.4  
+/* 
+//1.4  actualiza el valor de la variable globalBasePrice a 25000 . 
+actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad basePrice 
 
-let globalBasePrice = 10000;
+*/
+
+let globalBasePrice = 25000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+/* ITERACIÓN 3
+
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.
+
+*/
+
+//1.1 
+
+let mult = 10 * 5
+//console.log(mult)
+
+//1.2 
+
+let div = 10/2
+//console.log(div)
+
+//1.3 
+
+const remainder = 15%9
+
+
+//1.4 
+
+p = 10 
+j = 5
+o = j + p
+//console.log(o)
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+
+c = 10 
+m = 5 
+i = c * m 
+//console.log(i)
+
+
+/* ITERACIÓN 4
+
+1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+*/ 
+
+//1.1 
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+//console.log(avengers[0])
+
+//1.2 
+
+avengers [0] = "IRONMAN"
+//console.log(avengers[0])
+
+//1.3
+
+
