92bf42d9c7b2459019b19ca7315e8b584ebfef6c
InesMedem <inesmedem93@gmail.com>
2024-01-11 18:01:19 +0100

    Iteration#1

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -1,28 +1,39 @@
-/* ITERACIÓN 1
+// !
+// ?
+// *
+
+/* 1 ----------------------------------------------------------------------------------------------
+
+ITERACIÓN 1
+
 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
-1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.  */ 
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.  
 
-//1.1
+---------------------------------------------------------------------------------------------- */ 
+
+//!1.1
 
 let myFavoriteHero = "Hulk"; 
 
-//1.2 
+//!1.2 
 
 let x = 50
 
-//1.3
+//!1.3
 
 let h = 5 
 let y = 10
 
-//1.4 
+//!1.4 
 
 z = h + y 
 //console.log(z)
 
-/* ITERACIÓN 2
+/* 2 ----------------------------------------------------------------------------------------------
+
+ITERACIÓN 2
 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = {name: 'Jack Sparrow', age: 10};
 
@@ -45,34 +56,43 @@ let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
-*/
+---------------------------------------------------------------------------------------------- */
 
-// 1.1 
+//! 1.1 
 
 const character = {name: 'Jack Sparrow', age: 10};
 
-// 1.2
+//! 1.2
 
     firstName = 'Jon'; 
+    
 	lastName = 'Snow'
 	age = 24
 
-    console.log("Soy " + firstName+ " " + lastName + ", tengo " + age + " y me gustan los lobos.")  
+   // console.log("Soy " + firstName+ " " + lastName + ", tengo " + age + " y me gustan los lobos.")  
+   console.log(`Soy ${firstName, lastName} tengo ${age} y me gustan los lobos.`)
+   
 
-// 1.3 
+//! 1.3 
 
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
 //console.log(toy1.price + toy2.price)
 
-//1.4   
+//!1.4   
 
 let globalBasePrice = 25000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+
+const car1 = 
+{name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
-/* ITERACIÓN 3
+console.log(globalBasePrice)
+
+/* 3 ----------------------------------------------------------------------------------------------
+
+ITERACIÓN 3
 
 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
 
@@ -86,30 +106,30 @@ teniendo dos variables p = 10 y j = 5.
 1.5 Usa el correcto operador de asignación que resultará en i = 50,
 teniendo dos variables c = 10 y m = 5.
 
-*/
+---------------------------------------------------------------------------------------------- */
 
-//1.1 
+//!1.1 
 
 let mult = 10 * 5
 //console.log(mult)
 
-//1.2 
+//! 1.2 
 
 let div = 10/2
 //console.log(div)
 
-//1.3 
+//! 1.3 
 
 const remainder = 15%9
 
-//1.4 
+//! 1.4 
 
 p = 10 
 j = 5
 o = j + p
 //console.log(o)
 
-// 1.5 
+//! 1.5 
 
 c = 10 
 m = 5 
@@ -117,7 +137,9 @@ i = c * m
 //console.log(i)
 
 
-/* ITERACIÓN 4
+/* 4 ----------------------------------------------------------------------------------------------
+
+ITERACIÓN 4
 
 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
@@ -138,23 +160,23 @@ const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lap
 1.6 Elimina el segundo elemento del array y muestra el array por consola.
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 
-*/ 
+---------------------------------------------------------------------------------------------- */ 
 
-//1.1 
+//! 1.1 
 
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 //console.log(avengers[0]) 
 
-//1.2 
+//! 1.2 
 
 avengers [0] = "IRONMAN" 
 //console.log(avengers[0])
 
-//1.3 
+//! 1.3 
 
 console.log(avengers.length)
 
-//1.4 
+//! 1.4 
 
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 
@@ -164,9 +186,10 @@ const threeMore = rickAndMortyCharacters.pop()
 console.log(rickAndMortyCharacters)
 console.log(threeMore)
 
-//1.5 
+//! 1.5 
 
-/*
+/* 
+
 rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 const fourr = rickAndMortyCharacters.shift()
 const fivee = rickAndMortyCharacters.pop()
@@ -175,10 +198,66 @@ console.log(fourr, fivee)
 
 */
 
-//1.6 
+//! 1.6 
 
 const fourr = rickAndMortyCharacters[1]
 console.log(fourr)
 
 
+/* 5 ----------------------------------------------------------------------------------------------
+
+ITERACIÓN 5
+
+---------------------------------------------------------------------------------------------- */
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+
+if (number2/number1 == 2) {
+	console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2 ) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if (number3*5 == number1) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (number3*5 == number1 && number1*2 == number2) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (number2/2 == number1 ||  number1/5 == number3) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
+
+/* ----------------------------------------------------------------------------------------------
+
+ITERACIÓN 6
+
+1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+cuando el resto del numero dividido entre 2 sea 0.
+
+1.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+y cambia el mensaje en la décima vuelta a 'Dormido!'.
+
+---------------------------------------------------------------------------------------------- */
+
+//! 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+//!1.2
+
+//!1.3 
 
---
+++ JS/basicAlgorithms/test.js
@@ -0,0 +1,36 @@
+/*
+
+1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+basePrice más el valor de la variable globalBasePrice.
+
+*/
+//!1.4   
+
+let globalBasePrice = 25000;
+
+
+const car1 = {
+
+    name: 'BMW m&m', 
+    basePrice: 50000, 
+    finalPrice: 60000
+
+};
+let newPriceCar1 = car1.basePrice
+console.log(newPriceCar1)
+
+
+
+
+
+
+
+
+
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+//console.log(globalBasePrice)
+
+
+
