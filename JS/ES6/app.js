e2ece64ef205e0049fe9bd99e991e3fe37ebcedb
InesMedem <inesmedem93@gmail.com>
2024-01-21 14:09:19 +0100

    avances en ES6

--- JS/ES6/app.js
+++ JS/ES6/app.js
@@ -17,9 +17,8 @@ result2 = myFunction(1);
 result3 = myFunction(1, 1);
 
 //! Iteración #2: Destructuring
-/**2.1 crea variables en base a las propiedades del objeto usando object destructuring
- * e imprimelas por consola. Cuidado, no hace falta hacer destructuring del array, solo del objeto. *
- */
+/* 2.1 crea variables en base a las propiedades del objeto usando object destructuring
+e imprimelas por consola. Cuidado, no hace falta hacer destructuring del array, solo del objeto.*/
 
 const game = {
 	title: "The last us 2",
@@ -32,30 +31,34 @@ console.log("Iteración #2 2.1 🚀 ~ title:", title);
 console.log("Iteración #2 2.1 🚀 ~ gender:", gender);
 console.log("Iteración #2 2.1 🚀 ~ year:", year);
 
-/*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
+/* 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
-imprimelo por consola.*/
+imprimelo por consola. */
 
 const fruits = ["Banana", "Strawberry", "Orange"];
+const [fruit1, fruit2, fruit3] = fruits;
+console.log("Iteración #2 2.2 🚀 ~ fruit3:", fruit3);
+console.log("Iteración #2 2.2 🚀 ~ fruit2:", fruit2);
+console.log("Iteración #2 2.2 🚀 ~ fruit1:", fruit1);
 
-let fruit1;
-let fruit2;
-let fruit3;
-
-/** 
-
-2.3 En base al siguiente javascript, usa destructuring para crear 2 
-variables igualandolo a la función e imprimiendolo por consola.
+/* 2.3 En base al siguiente javascript, usa destructuring para crear 2 
+variables igualandolo a la función e imprimiendolo por consola.*/
 
 const animalFunction = () => {
-    return {name: 'Bengal Tiger', race: 'Tiger'}
+	return { name: "Bengal Tiger", race: "Tiger" };
 };
 
-2.4 En base al siguiente javascript, usa destructuring para crear las 
+const { name, race } = animalFunction();
+console.log("Iteración #2 2.3 🚀 ~ race:", race);
+console.log("Iteración #2 2.3 🚀 ~ name:", name);
+
+/* 2.4 En base al siguiente javascript, usa destructuring para crear las 
 variables name y itv con sus respectivos valores. Posteriormente crea 
 3 variables usando igualmente el destructuring para cada uno de los años 
-y comprueba que todo esta bien imprimiendolo.
+y comprueba que todo esta bien imprimiendolo. */
 
-const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
- * 
- */
+const car = { name2: "Mazda 6", itv: [2015, 2011, 2020] };
+const { name2, itv } = car;
+console.log("Iteración #2 2.4 🚀 ~ car:", car);
+const [yr1, yr2, yr3] = itv;
+console.log("Iteración #2 2.4 🚀 ~ itv:", itv);
