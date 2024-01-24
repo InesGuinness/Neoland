7ebf4c80977ac277dc6a6cfa358ebd557c840c68
InesMedem <inesmedem93@gmail.com>
2024-01-24 08:29:49 +0100

    ultimos ejercicios ES6

--- JS/ES6/app.js
+++ JS/ES6/app.js
@@ -219,31 +219,21 @@ const filtersByLegend = streamers.filter((streamer) => {
 	}
 });
 
-/*
-
-5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+/* 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 los streamers que incluyan la palabra introducida en el input. De esta forma, si 
 introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
-introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
-const streamers = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-];
+introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.*/
 
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-             <title>Document</title>
-</head>
-<body>
-  <input type="text" data-function="toFilterStreamers"/>
-</body>
-</html>
+//const toFilterStreamers = (text) => {	streamers.filter(text);}; return
+
+const toFilterStreamers = (streamers, text) => {
+	return streamers.filter((streamer) => streamer.name.includes(text));
+};
+
+response = toFilterStreamers(streamers, "i");
+console.log("Iteración #5 5. 🚀 ~ response:", response);
+
+/*
 
 5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
 los streamers que incluyan la palabra introducida en el input. De esta forma, si 
